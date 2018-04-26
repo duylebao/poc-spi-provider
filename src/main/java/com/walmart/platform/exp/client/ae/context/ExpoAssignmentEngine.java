6c3078031eef9796a579deeff51e61f0b12ee170
Duy Le <dle@walmartlabs.com>
2018-04-26 08:47:27 -0700

    initial

---
+++ src/main/java/com/walmart/platform/exp/client/ae/context/ExpoAssignmentEngine.java
@@ -0,0 +1,36 @@
+package com.walmart.platform.exp.client.ae.context;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import com.walmart.platform.exp.client.ae.context.model.AssignmentTreatment;
+import com.walmart.platform.exp.client.ae.metadata.model.ExpoAssignmentMetadata;
+
+public class ExpoAssignmentEngine implements IExpoAssignmentEngine{
+    
+    @SuppressWarnings("serial")
+    public Optional<AssignmentTreatment> getAssignmentTreatment(ExpoAssignmentMetadata expoAssignmentMetadata, String cid) throws Exception {        
+        AssignmentTreatment treatment = new AssignmentTreatment();
+        treatment.setAssignments("var1|var3");
+        Map<String, String> map = new HashMap<String,String>()
+                {
+                    {
+                        put("color","blue");
+                        put("size","10");
+                    }
+                };
+        treatment.setTreatments(map);
+        
+        AssignmentTreatment other = new AssignmentTreatment();
+        other.setAssignments("var2|var4");
+        Map<String, String> otherMap = new HashMap<String,String>()
+                {
+                    {
+                        put("size","100");
+                    }
+                };
+        other.setTreatments(otherMap);
+        return "dle".equalsIgnoreCase(cid) ? Optional.of(treatment) : Optional.of(other);
+    }
+
+}
